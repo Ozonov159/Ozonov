a8b83913ebd785698d61042e2ab29faa258fdfb1
Ozonov159 <sa.tebyakin@gmail.com>
2026-04-09 12:22:09 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+print("BOT IS STARTING...")
 
+import asyncio
+
+async def main():
+    try:
+        print("RUNNING POLLING...")
+        await dp.start_polling(bot)
+    except Exception as e:
+        print("ERROR:", e)
+
+if __name__ == "__main__":
+    asyncio.run(main())
